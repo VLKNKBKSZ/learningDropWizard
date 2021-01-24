705537e54408f9d7ec8ec789372e589978ffb581
VLKNKBKSZ <kabaksizvolkan@hotmail.com>
2021-01-24 11:17:47 +0100

    added config class, yml  and main application entry with resource endpoint

---
+++ src/main/java/HelloWorldApplication.java
@@ -0,0 +1,22 @@
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) throws Exception {
+
+    }
+}
